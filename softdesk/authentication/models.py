63fb8811f0261e029bc569872377e3ffe445986e
Sandy Ludosky <sludosky@xebia.fr>
2023-07-21 21:38:24 +0200

    Abstract user

--- softdesk/authentication/models.py
+++ softdesk/authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    """
+    Custom User Model
+    """
+    age = models.PositiveIntegerField(blank=True, null=True)
+    can_be_shared = models.BooleanField(default=False)
+    can_be_contacted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
